3bafac17f399945dba249ccfef82b007a40fe2f9
me-cafebabe <me_cafebabe@hotmail.com>
2024-12-03 17:09:08 +0300

    sm6375-common: Setup DeviceAsWebcam
    Change-Id: I1d4f9b6b18e1c78afda9b9ccb981f300a3258f19

--- common.mk
+++ common.mk
@@ -28,6 +28,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    DeviceAsWebcamCommon \
     FrameworksResCommon \
     FrameworksResProduct \
     FrameworksResTarget \
