f64a3b6c3ba57cb06e8145d2ddd9778da980b524
TRIDEV099 <tridev09@zohomail.in>
2026-04-04 19:07:51 +0530

    sm6375-common: init @ProjectMatrixx

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,7 +109,7 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 BOARD_USES_METADATA_PARTITION := true
 
 # Partitions
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/matrixx/config/BoardConfigReservedSize.mk
 BOARD_EROFS_COMPRESSOR := lz4
 BOARD_EROFS_PCLUSTER_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
